d4e620dd137d93b32f55d867bf7bc46f7fd613ff
Shichao Wang <swang206@myseneca.ca>
2020-06-17 19:00:58 -0400

    addCustomer()

--- routes/customers.js
+++ routes/customers.js
@@ -1,5 +1,56 @@
 const router = require("express").Router();
 let Customer = require("../models/customer.model");
+const e = require("express");
+
+addCustomer = async function(obj){
+  const regExpEmail = RegExp(
+    /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/
+  );
+  
+  const regExpPhone = RegExp(
+    /^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$/
+  );
+  
+  const regExpPassword = RegExp(
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,32}$/
+  );
+  const firstName = obj.firstname;
+  const lastName = obj.lastname;
+  const email = obj.email;
+  const phoneNumber = obj.phonenumber;
+  const password = obj.password;
+
+  let messages = {};
+  const newCustomer = new Customer({
+    firstName,
+    astName,
+    email,
+    phoneNumber,
+    password,
+  });
+  if(firstname.length < 1){
+    messages.firstName = "First name should have at least one char"
+  }
+  if(lastName.length < 1){
+    messages.lastName = "First name should have at least one char"
+  }
+  if(!regExpEmail.test(email)){
+    messages.email = "Incorrect email format"
+  }
+  if(!regExpPassword.test(password)){
+    messages.password = "Password does not satisfy the password policy"
+  }
+  if(!regExpPhone.test(phonenumber)){
+    messages.phoneNumber = "Incorrect phone number"
+  }
+  if(messages !== {}) throw messages;
+  
+  return await newCustomer.save()
+}
+
+
+
+
 
 //get request (/customers)
 router.route("/").get((req, res) => {
@@ -11,24 +62,9 @@ router.route("/").get((req, res) => {
 
 // post request (/customers/add)
 router.route("/add").post((req, res) => {
-  const firstName = req.body.firstname;
-  const lastName = req.body.lastname;
-  const email = req.body.email;
-  const phoneNumber = req.body.phonenumber;
-  const password = req.body.password;
-
-  const newCustomer = new Customer({
-    firstName,
-    lastName,
-    email,
-    phoneNumber,
-    password,
-  });
-
-  newCustomer
-    .save()
-    .then(() => res.json("Customer Added"))
-    .catch((err) => res.status(400).json("Rrror: " + err));
+  addCustomer({firstName: req.body.firstName, lastName: req.body.lastName, email: req.body.email, phoneNumber: req.body.phoneNumber, password: req.body.phoneNumber})
+  .then(()=>res.json({errcode: 0, messages: "success"}))
+  .catch((err) => res.json({errcode: 1, messages: err}))
 });
 
 router.route("/:id").get((req, res) => {
