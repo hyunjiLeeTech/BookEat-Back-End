c2509c8fecbe96ba8da8231362a2322ce54d8a90
HyunjiLee <hyunji.lee.tech@gmail.com>
2020-06-27 14:52:45 -0400

    Completed restaurant signup with validation

--- routes/restaurnatOwners.js
+++ routes/restaurnatOwners.js
@@ -3,7 +3,10 @@ let RestaurantOwner = require("../models/restaurantOwner.model");
 let Account = require("../models/account.model");
 let Address = require("../models/address.model");
 const Restaurant = require("../models/restaurnat.model");
-const Customer = require("../models/customer.model");
+
+let findAccountByEmailAsyc = async function (email) {
+  return await Account.find({ email: email });
+};
 
 //get request (/customers)
 router.route("/").get((req, res) => {
@@ -13,10 +16,110 @@ router.route("/").get((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
-// post request (/customers/add)
-router.route("/add").post((req, res) => {
+//restaurant signup
+let addRestaurantOwnerAsync = async function (obj) {
+  const regExpEmail = RegExp(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/);
+
+  const regExpPhone = RegExp(
+    /^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$/
+  );
+
+  const regExpPassword = RegExp(
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,32}$/
+  );
+
+  const regExpPostalCode = RegExp(/^([A-Za-z]\d[A-Za-z][-]?\d[A-Za-z]\d)$/);
+
+  const regExpBusinessNum = RegExp(/^[0-9]{9}\s+[A-Za-z]{2}\s+[0-9]{4}$/);
+
   //account
-  const userTypeId = 2;
+  const userTypeId = 2; // restaurant owner
+  const password = obj.password;
+  const email = obj.email;
+
+  //restaurantOwner
+  // this is for restaurant
+
+  //address
+  const province = obj.province;
+  const streetNumber = obj.streetNumber;
+  const streetName = obj.streetName;
+  const postalCode = obj.postalCode;
+  const city = obj.city;
+
+  //restaurant
+  const resName = obj.resName;
+  const businessNum = obj.businessNum;
+  const phoneNumber = obj.phoneNumber;
+
+  const newAccount = new Account({
+    email,
+    password,
+    userTypeId,
+  });
+
+  const newAddress = new Address({
+    province,
+    streetName,
+    streetNumber,
+    postalCode,
+    city,
+  });
+
+  let message = "";
+  if ((await findAccountByEmailAsyc(email)).length > 0) {
+    message = "This email is already registered";
+    throw message;
+  }
+
+  if (!regExpEmail.test(email)) {
+    message = "Incorrect email format";
+    throw message;
+  }
+
+  if (!regExpPhone.test(phoneNumber)) {
+    message = "Incorrect phone number";
+    throw message;
+  }
+
+  if (!regExpBusinessNum.test(businessNum)) {
+    message = "Incorrect business number";
+    throw message;
+  }
+
+  if (!regExpPostalCode.test(postalCode)) {
+    message = "Incorrect postal code";
+    throw message;
+  }
+
+  // if (!regExpPassword.test(password)) {
+  //   message = "Incorrect password";
+  //   throw message;
+  // }
+
+  let acnt = await newAccount.save();
+  let address = await newAddress.save();
+
+  const newRestaurantOwner = new RestaurantOwner({
+    account: acnt._id,
+  });
+
+  let restOwner = await newRestaurantOwner.save();
+
+  const newRestaurant = new Restaurant({
+    resName,
+    phoneNumber,
+    businessNum,
+    restaurantOwnerId: restOwner._id,
+    addressId: address._id,
+  });
+
+  return await newRestaurant.save();
+};
+
+router.post("/add", (req, res) => {
+  //account
+  const userTypeId = 2; // restaurant owner
   const password = req.body.password;
   const email = req.body.email;
 
@@ -34,51 +137,94 @@ router.route("/add").post((req, res) => {
   const resName = req.body.resName;
   const businessNum = req.body.businessNum;
   const phoneNumber = req.body.phoneNumber;
-
-  const newAccount = new Account({
-    email,
-    password,
+  var obj = {
     userTypeId,
-  });
-
-  const newAddress = new Address({
+    password,
+    email,
     province,
-    streetName,
     streetNumber,
+    streetName,
     postalCode,
     city,
-  });
-
-  newAccount
-    .save()
-    .then((account) => {
-      res.json("Account Added");
-
-      const accountId = account._id;
-      let resOwnerId, adrId;
-
-      const newRestaurantOwner = new RestaurantOwner({
-        account: accountId,
-      });
-
-      newRestaurantOwner.save().then((resOwner) => {
-        resOwnerId = resOwner._id;
-
-        newAddress.save().then((address) => {
-          adrId = address._id;
-          const newRestaurant = new Restaurant({
-            resName,
-            phoneNumber,
-            businessNum,
-            restaurantOwnerId: resOwnerId,
-            addressId: adrId,
-          });
-
-          newRestaurant.save();
-        });
-      });
+    resName,
+    businessNum,
+    phoneNumber,
+  };
+  addRestaurantOwnerAsync(obj)
+    .then(() => {
+      res.json({ errcode: 0, errmsg: "success" });
     })
-    .catch((err) => res.status(400).json("Error: " + err));
+    .catch((err) => {
+      res.json({ errcode: 1, errmsg: err });
+    });
 });
+
+// // post request (/customers/add)
+// router.route("/add").post((req, res) => {
+//   //account
+//   const userTypeId = 2; // restaurant owner
+//   const password = req.body.password;
+//   const email = req.body.email;
+
+//   //restaurantOwner
+//   // this is for restaurant
+
+//   //address
+//   const province = req.body.province;
+//   const streetNumber = req.body.streetNumber;
+//   const streetName = req.body.streetName;
+//   const postalCode = req.body.postalCode;
+//   const city = req.body.city;
+
+//   //restaurant
+//   const resName = req.body.resName;
+//   const businessNum = req.body.businessNum;
+//   const phoneNumber = req.body.phoneNumber;
+
+//   const newAccount = new Account({
+//     email,
+//     password,
+//     userTypeId,
+//   });
+
+//   const newAddress = new Address({
+//     province,
+//     streetName,
+//     streetNumber,
+//     postalCode,
+//     city,
+//   });
+
+//   newAccount
+//     .save()
+//     .then((account) => {
+//       res.json("Account Added");
+
+//       const accountId = account._id;
+//       let resOwnerId, adrId;
+
+//       const newRestaurantOwner = new RestaurantOwner({
+//         account: accountId,
+//       });
+
+//       newRestaurantOwner.save().then((resOwner) => {
+//         resOwnerId = resOwner._id;
+
+//         newAddress.save().then((address) => {
+//           adrId = address._id;
+//           const newRestaurant = new Restaurant({
+//             resName,
+//             phoneNumber,
+//             businessNum,
+//             restaurantOwnerId: resOwnerId,
+//             addressId: adrId,
+//           });
+
+//           newRestaurant.save();
+//         });
+//       });
+//     })
+//     .catch((err) => res.status(400).json("Error: " + err));
+// });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 let Customer = require("./models/customer.model");
 let Account = require("./models/account.model");
+let RestaurantOwner = require("./models/restaurantOwner.model");
+let Restaurant = require("./models/restaurnat.model");
+let Address = require("./models/address.model");
 
 app.use(cors());
 app.use(express.json());
@@ -35,6 +38,7 @@ connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
 
+// for user signup
 let findAccountByEmailAsyc = async function (email) {
   return await Account.find({ email: email });
 };
@@ -43,6 +47,7 @@ let findCustomerByPhoneNumberAsync = async function (phonenumber) {
   return await Customer.find({ phoneNumber: phonenumber });
 };
 
+// for customer signup
 let addCustomerAsync = async function (obj) {
   const regExpEmail = RegExp(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/);
 
@@ -110,6 +115,7 @@ const categoryRouter = require("./routes/category");
 const accountRouter = require("./routes/account");
 const restaurantRouter = require("./routes/restaurant");
 const addressRouter = require("./routes/address");
+const RestaurnatOwner = require("./models/restaurantOwner.model");
 
 app.use(
   "/customers",
@@ -196,6 +202,149 @@ app.post("/customersignup", (req, res) => {
 //   };
 //   res.json(returnData);
 // });
+
+//restaurant signup
+let addRestaurantOwnerAsync = async function (obj) {
+  const regExpEmail = RegExp(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/);
+
+  const regExpPhone = RegExp(
+    /^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$/
+  );
+
+  const regExpPassword = RegExp(
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,32}$/
+  );
+
+  const regExpPostalCode = RegExp(/^([A-Za-z]\d[A-Za-z][-]?\d[A-Za-z]\d)$/);
+
+  const regExpBusinessNum = RegExp(/^[0-9]{9}\s+[A-Za-z]{2}\s+[0-9]{4}$/);
+
+  //account
+  const userTypeId = 2; // restaurant owner
+  const password = obj.password;
+  const email = obj.email;
+
+  //restaurantOwner
+  // this is for restaurant
+
+  //address
+  const province = obj.province;
+  const streetNumber = obj.streetNumber;
+  const streetName = obj.streetName;
+  const postalCode = obj.postalCode;
+  const city = obj.city;
+
+  //restaurant
+  const resName = obj.resName;
+  const businessNum = obj.businessNum;
+  const phoneNumber = obj.phoneNumber;
+
+  const newAccount = new Account({
+    email,
+    password,
+    userTypeId,
+  });
+
+  const newAddress = new Address({
+    province,
+    streetName,
+    streetNumber,
+    postalCode,
+    city,
+  });
+
+  let message = "";
+  if ((await findAccountByEmailAsyc(email)).length > 0) {
+    message = "This email is already registered";
+    throw message;
+  }
+
+  if (!regExpEmail.test(email)) {
+    message = "Incorrect email format";
+    throw message;
+  }
+
+  if (!regExpPhone.test(phoneNumber)) {
+    message = "Incorrect phone number";
+    throw message;
+  }
+
+  if (!regExpBusinessNum.test(businessNum)) {
+    message = "Incorrect business number";
+    throw message;
+  }
+
+  if (!regExpPostalCode.test(postalCode)) {
+    message = "Incorrect postal code";
+    throw message;
+  }
+
+  // if (!regExpPassword.test(password)) {
+  //   message = "Incorrect password";
+  //   throw message;
+  // }
+
+  let acnt = await newAccount.save();
+  let address = await newAddress.save();
+
+  const newRestaurantOwner = new RestaurantOwner({
+    account: acnt._id,
+  });
+
+  let restOwner = await newRestaurantOwner.save();
+
+  const newRestaurant = new Restaurant({
+    resName,
+    phoneNumber,
+    businessNum,
+    restaurantOwnerId: restOwner._id,
+    addressId: address._id,
+  });
+
+  return await newRestaurant.save();
+};
+
+app.post("/restaurantownersignup", (req, res) => {
+  //account
+  const userTypeId = 2; // restaurant owner
+  const password = req.body.password;
+  const email = req.body.email;
+
+  //restaurantOwner
+  // this is for restaurant
+
+  //address
+  const province = req.body.province;
+  const streetNumber = req.body.streetNumber;
+  const streetName = req.body.streetName;
+  const postalCode = req.body.postalCode;
+  const city = req.body.city;
+
+  //restaurant
+  const resName = req.body.resName;
+  const businessNum = req.body.businessNum;
+  const phoneNumber = req.body.phoneNumber;
+  var obj = {
+    userTypeId,
+    password,
+    email,
+    province,
+    streetNumber,
+    streetName,
+    postalCode,
+    city,
+    resName,
+    businessNum,
+    phoneNumber,
+  };
+  addRestaurantOwnerAsync(obj)
+    .then(() => {
+      res.json({ errcode: 0, errmsg: "success" });
+    })
+    .catch((err) => {
+      res.json({ errcode: 1, errmsg: err });
+    });
+});
 
 app.get(
   "/testAuth",
