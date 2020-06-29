1922c60bdce9f42701d9a3a4f6b413b5ac56411a
HyunjiLee <hyunji.lee.tech@gmail.com>
2020-06-29 13:32:09 -0400

    Created manager schema

---
+++ routes/manager.js
@@ -0,0 +1,48 @@
+const router = require("express").Router();
+let Account = require("../models/account.model");
+let Restaurant = require("../models/restaurnat.model");
+let Manager = require("../models/manager.model");
+
+router.route("/").get((req, res) => {
+  console.log("Accessing /manager");
+  Manager.find()
+    .then((manager) => res.json(manager))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/add").post((req, res) => {
+  console.log("Accessing /manager/add");
+
+  //account info
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //manager info
+  const firstname = req.body.firstname;
+  const lastname = req.body.lastname;
+  const phonenumber = req.body.phonenumber;
+
+  obj = {
+    email,
+    password,
+    firstname,
+    lastname,
+    phonenumber,
+  };
+
+  addManagerAsync(obj);
+});
+
+let addManagerAsync = async function (obj) {
+  //account info
+  const email = obj.email;
+  const password = obj.password;
+  const userTypeId = 3; // manager user type: 3
+
+  //manager info
+  const firstname = obj.firstname;
+  const lastname = obj.lastname;
+  const phonenumber = obj.phonenumber;
+};
+
+module.exports = router;
