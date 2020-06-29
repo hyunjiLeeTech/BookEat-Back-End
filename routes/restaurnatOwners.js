33164e792f2a9e1e9d44f6183d38d94e81a8914b
HyunjiLee <hyunji.lee.tech@gmail.com>
2020-06-29 16:00:38 -0400

    Removed unnecessary codes

--- routes/restaurnatOwners.js
+++ routes/restaurnatOwners.js
@@ -28,7 +28,6 @@ router.route("/getrestaurantinfo").get((req, res) => {
         res.json(restaurant);
       });
   });
-  //console.log(req.user);
 });
 
 //restaurant signup
@@ -173,73 +172,5 @@ router.post("/add", (req, res) => {
       res.json({ errcode: 1, errmsg: err });
     });
 });
-
-// // post request (/customers/add)
-// router.route("/add").post((req, res) => {
-//   //account
-//   const userTypeId = 2; // restaurant owner
-//   const password = req.body.password;
-//   const email = req.body.email;
-
-//   //restaurantOwner
-//   // this is for restaurant
-
-//   //address
-//   const province = req.body.province;
-//   const streetNumber = req.body.streetNumber;
-//   const streetName = req.body.streetName;
-//   const postalCode = req.body.postalCode;
-//   const city = req.body.city;
-
-//   //restaurant
-//   const resName = req.body.resName;
-//   const businessNum = req.body.businessNum;
-//   const phoneNumber = req.body.phoneNumber;
-
-//   const newAccount = new Account({
-//     email,
-//     password,
-//     userTypeId,
-//   });
-
-//   const newAddress = new Address({
-//     province,
-//     streetName,
-//     streetNumber,
-//     postalCode,
-//     city,
-//   });
-
-//   newAccount
-//     .save()
-//     .then((account) => {
-//       res.json("Account Added");
-
-//       const accountId = account._id;
-//       let resOwnerId, adrId;
-
-//       const newRestaurantOwner = new RestaurantOwner({
-//         account: accountId,
-//       });
-
-//       newRestaurantOwner.save().then((resOwner) => {
-//         resOwnerId = resOwner._id;
-
-//         newAddress.save().then((address) => {
-//           adrId = address._id;
-//           const newRestaurant = new Restaurant({
-//             resName,
-//             phoneNumber,
-//             businessNum,
-//             restaurantOwnerId: resOwnerId,
-//             addressId: adrId,
-//           });
-
-//           newRestaurant.save();
-//         });
-//       });
-//     })
-//     .catch((err) => res.status(400).json("Error: " + err));
-// });
 
 module.exports = router;
