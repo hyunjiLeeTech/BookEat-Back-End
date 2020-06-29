00e642a3599cb5b787e951045ebc45d51bbd040c
HyunjiLee <hyunji.lee.tech@gmail.com>
2020-06-29 13:32:44 -0400

    Created manager schema

---
+++ models/manager.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const managerSchema = new Schema({
+  firstname: {
+    type: String,
+    required: true,
+    maxLength: 32,
+  },
+  lastname: {
+    type: String,
+    required: true,
+    maxLength: 32,
+  },
+  phonenumber: {
+    type: Number,
+    required: false,
+    minlength: 10,
+    maxlength: 10,
+  },
+  restaurantId: { type: Schema.Types.ObjectId, ref: "Restaurant" },
+  accountId: { type: Schema.Types.ObjectId, ref: "Account" },
+});
+
+const Manager = mongoose.model("Manager", managerSchema);
+
+Manager.createIndexes();
+
+module.exports = Manager;
