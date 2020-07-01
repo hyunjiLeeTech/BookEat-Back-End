30611117064b13e770d1070a4781099c826ee783
HyunjiLee <hyunji.lee.tech@gmail.com>
2020-07-01 18:48:05 -0400

    Added timestamp in manager table

--- models/manager.model.js
+++ models/manager.model.js
@@ -1,26 +1,31 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const managerSchema = new Schema({
-  firstname: {
-    type: String,
-    required: true,
-    maxLength: 32,
+const managerSchema = new Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+      maxLength: 32,
+    },
+    lastname: {
+      type: String,
+      required: true,
+      maxLength: 32,
+    },
+    phonenumber: {
+      type: Number,
+      required: false,
+      minlength: 10,
+      maxlength: 10,
+    },
+    restaurantId: { type: Schema.Types.ObjectId, ref: "Restaurant" },
+    accountId: { type: Schema.Types.ObjectId, ref: "Account" },
   },
-  lastname: {
-    type: String,
-    required: true,
-    maxLength: 32,
-  },
-  phonenumber: {
-    type: Number,
-    required: false,
-    minlength: 10,
-    maxlength: 10,
-  },
-  restaurantId: { type: Schema.Types.ObjectId, ref: "Restaurant" },
-  accountId: { type: Schema.Types.ObjectId, ref: "Account" },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Manager = mongoose.model("Manager", managerSchema);
 
