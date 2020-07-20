4d30904a36740e1f3e83016b250db2af019b7613
HyunjiLee <hyunji.lee.tech@gmail.com>
2020-07-19 20:49:01 -0400

    Completed add discount

---
+++ routes/discount.js
@@ -0,0 +1,53 @@
+const router = require("express").Router();
+let Discount = require("../models/discount.model");
+let RestaurantOwner = require("../models/restaurantOwner.model");
+let Restaurant = require("../models/restaurnat.model");
+
+router.route("/adddiscount").post(async (req, res) => {
+    console.log("Accessing discount/adddiscount");
+
+    var obj = {
+        accountId: req.user._id,
+        percent: req.body.percent,
+        descript: req.body.descript,
+    }
+
+    addDiscountAsync(obj).then(() => {
+        res.json({ errcode: 0, errmsg: "success" })
+    }).catch((err) => {
+        res.json({ errcode: 1, errmsg: err })
+    })
+})
+
+async function findRestaurantByIdAsync(id) {
+    restaurantOwner = await RestaurantOwner.findOne({ account: id })
+
+    return await Restaurant.findOne({ restaurantOwnerId: restaurantOwner._id });
+}
+
+async function addDiscountAsync(obj) {
+    const accountId = obj.accountId;
+    const percent = obj.percent;
+    const descript = obj.descript;
+    const isActive = true;
+
+    let message = "";
+
+    if (percent > 100 || percent < 0) {
+        message = "Discount range: 0-100";
+        throw message;
+    }
+
+    let restaurant = await findRestaurantByIdAsync(accountId);
+
+    const newDiscount = new Discount({
+        percent,
+        descript,
+        isActive,
+        restaurantId: restaurant._id
+    })
+
+    return await newDiscount.save();
+}
+
+module.exports = router;
