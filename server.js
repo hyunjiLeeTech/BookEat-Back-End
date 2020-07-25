f6f669eabb47db708d138bd80c51dfb811b33c62
HyunjiLee <hyunji.lee.tech@gmail.com>
2020-07-25 18:15:38 -0400

    Resolved 413 error

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ let Address = require("./models/address.model");
 let Manager = require("./models/manager.model");
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(
   expressSession({
     secret: "BookEatAwesome",
